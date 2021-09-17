6260f33e5ab3f20e9374bec545409694a73875b5
Francois Daoust <fd@tidoust.net>
2021-09-17 11:27:02 +0200

    Semver only, default is release, add major versions and README/LICENSE

--- tools/publish-lib.js
+++ tools/publish-lib.js
@@ -6,14 +6,41 @@
 
 const util = require('util');
 const fs = require('fs');
+const semver = require('semver');
 const execFile = util.promisify(require('child_process').execFile);
+
+const lib = 'i-slide';
+
+async function writeLib(ref, filename) {
+  filename = filename || `${lib}-${ref}`;
+  const { stdout: showOut } = await execFile('git', ['show', `${ref}:${lib}.js`]);
+  fs.writeFileSync(`${filename}.js`, showOut, 'utf8');
+}
 
 async function main() {
   console.log('Retrieve list of tags...');
   const { stdout: tagOut } = await execFile('git', ['tag', '--sort=-v:refname']);
-  const tags = tagOut.split(/[\r\n]/).filter(tag => !!tag);
-  const lastTag = tags[0] ?? 'last commit';
+  const tags = tagOut.split(/[\r\n]/)
+    .filter(tag => !!tag)
+    .filter(tag => semver.valid(semver.clean(tag)));
+  const lastTag = tags[0] ?? 'main';
   console.log(`  ${tags.length} tags found`);
+
+  console.log('Find major versions...');
+  const majorVersions = tags
+    .map(tag => Object.assign({
+      major: semver.major(semver.clean(tag)),
+      tag
+    }))
+    .filter((item, pos, arr) =>
+      item.major !== null &&
+      arr.find(i => i.major === item.major) === item);
+  if (majorVersions.length) {
+    majorVersions.forEach(m => console.log(`  ${m.major} => ${m.tag}`));
+  }
+  else {
+    console.log('  no major version found');
+  }
 
   console.log('Lookup name of current branch...');
   const { stdout: branchOut } = await execFile('git', ['branch', '--show-current']);
@@ -26,14 +53,29 @@ async function main() {
 
   console.log('Create one lib file per tag...');
   for (const tag of tags) {
-    const { stdout: showOut } = await execFile('git', ['show', `${tag}:i-slide.js`]);
-    fs.writeFileSync(`i-slide-${tag}.js`, showOut, 'utf8');
+    await writeLib(tag, `${lib}-${semver.clean(tag)}`);
   }
   console.log('  done');
 
+  console.log('Create one lib file per major version...');
+  for (const version of majorVersions) {
+    await writeLib(version.tag, `${lib}-${version.major}`);
+  }
+  console.log('  done');
+
+  console.log('Add latest release...')
+  await writeLib(lastTag, lib);
+  console.log('  done');
+
   console.log('Add nightly version from main branch...')
-  const { stdout: mainOut } = await execFile('git', ['show', 'main:i-slide.js']);
-  fs.writeFileSync('i-slide.js', mainOut, 'utf8');
+  await writeLib('main', `${lib}-nightly`);
+  console.log('  done');
+
+  console.log('Update README and LICENSE if needed...');
+  for (const file of ['README.md', 'LICENSE']) {
+    const { stdout: fileOut } = await execFile('git', ['show', `${lastTag}:${file}`]);
+    fs.writeFileSync(file, fileOut, 'utf8');
+  }
   console.log('  done');
 
   console.log('Commit changes to gh-pages branch...');
