0487d8f9fb32410d7bfe7ae1fa1a46a9debf7e11
François Daoust <fd@w3.org>
2021-09-17 15:20:23 +0200

    Add workflow to publish one lib per tag to gh-pages branch (#11)
    The workflow runs whenever a tag gets added to the repository. It updates the
    gh-pages branch as needed with new versions of the JS library:
    - one file per version, e.g. `i-slide-1.0.0.js`
    - one file per major version, e.g. `i-slide-1.js`
    - the nightly version, named `i-slide-nightly.js`
    - the latest released (i.e. tagged) version, named `i-slide.js`
    
    Workflow runs whenever a push gets made to the main branch and whenever a tag gets added.
    
    It only takes semver tags into account.
    
    Workflow also updates the README and LICENSE documents in the gh-pages branch if needed.

---
+++ tools/publish-lib.js
@@ -0,0 +1,101 @@
+/**
+ * Create one JS library per tag and commit the result to the gh-pages branch
+ * 
+ * node tools/publish-tags.js
+ */
+
+const util = require('util');
+const fs = require('fs');
+const semver = require('semver');
+const execFile = util.promisify(require('child_process').execFile);
+
+const lib = 'i-slide';
+
+async function writeLib(ref, filename) {
+  filename = filename || `${lib}-${ref}`;
+  const { stdout: showOut } = await execFile('git', ['show', `${ref}:${lib}.js`]);
+  fs.writeFileSync(`${filename}.js`, showOut, 'utf8');
+}
+
+async function main() {
+  console.log('Retrieve list of tags...');
+  const { stdout: tagOut } = await execFile('git', ['tag', '--sort=-v:refname']);
+  const tags = tagOut.split(/[\r\n]/)
+    .filter(tag => !!tag)
+    .filter(tag => semver.valid(semver.clean(tag)));
+  const lastTag = tags[0] ?? 'main';
+  console.log(`  ${tags.length} tags found`);
+
+  console.log('Find major versions...');
+  const majorVersions = tags
+    .map(tag => Object.assign({
+      major: semver.major(semver.clean(tag)),
+      tag
+    }))
+    .filter((item, pos, arr) =>
+      item.major !== null &&
+      arr.find(i => i.major === item.major) === item);
+  if (majorVersions.length) {
+    majorVersions.forEach(m => console.log(`  ${m.major} => ${m.tag}`));
+  }
+  else {
+    console.log('  no major version found');
+  }
+
+  console.log('Lookup name of current branch...');
+  const { stdout: branchOut } = await execFile('git', ['branch', '--show-current']);
+  const branchName = branchOut.trim();
+  console.log(`  on branch ${branchName}`);
+
+  console.log('Switch to gh-pages branch...');
+  const { stdout: checkOut } = await execFile('git', ['checkout', 'gh-pages']);
+  console.log('  done');
+
+  console.log('Create one lib file per tag...');
+  for (const tag of tags) {
+    await writeLib(tag, `${lib}-${semver.clean(tag)}`);
+  }
+  console.log('  done');
+
+  console.log('Create one lib file per major version...');
+  for (const version of majorVersions) {
+    await writeLib(version.tag, `${lib}-${version.major}`);
+  }
+  console.log('  done');
+
+  console.log('Add latest release...')
+  await writeLib(lastTag, lib);
+  console.log('  done');
+
+  console.log('Add nightly version from main branch...')
+  await writeLib('main', `${lib}-nightly`);
+  console.log('  done');
+
+  console.log('Update README and LICENSE if needed...');
+  for (const file of ['README.md', 'LICENSE']) {
+    const { stdout: fileOut } = await execFile('git', ['show', `${lastTag}:${file}`]);
+    fs.writeFileSync(file, fileOut, 'utf8');
+  }
+  console.log('  done');
+
+  console.log('Commit changes to gh-pages branch...');
+  await execFile('git', ['add', 'i-slide*.js']);
+  const { stdout: diffOut } = await execFile('git', ['diff', '--staged', '--compact-summary']);
+  if (diffOut.trim()) {
+    console.log(diffOut);
+    await execFile('git', ['commit', '-m', `Update published libs for ${lastTag}`]);
+    console.log('  done');
+  }
+  else {
+    console.log('  nothing to commit');
+  }
+
+  console.log('Switch back to initial branch...');
+  await execFile('git', ['checkout', branchName]);
+  console.log('  done');
+}
+
+main().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
